1811cb79427bcf799f7abf4d959f4ac468b6fbea
Andrew Waweru <andyweru@gmail.com>
2018-10-02 11:30:15 +0300

    decouple and email configurations added

--- tribune/tribune/settings.py
+++ tribune/tribune/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-
+from decouple import config
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -24,6 +24,14 @@ SECRET_KEY = 'ocb9i*+j=+-fi9y!_dc2(m)b*#(d$s19(0!g@w5h76ccs+yxi!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+
+# Email configurations remember to install python-decouple
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 ALLOWED_HOSTS = []
 
