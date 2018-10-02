6ed9f7a5ceded53b021ee28bd5f9c02bbb3aee70
Andrew Waweru <andyweru@gmail.com>
2018-10-02 11:29:47 +0300

    send email

--- tribune/news/views.py
+++ tribune/news/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse,Http404
+from django.http import HttpResponse,Http404,HttpResponseRedirect
 import datetime as dt
-from .models import Article
+from .models import Article, NewsLetterRecipients
 from .forms import NewsLetterForm
+from .email import send_welcome_email
 
 # Create your views here.
 def welcome(request):
@@ -16,7 +17,15 @@ def news_today(request):
     if request.method == 'POST':
         form = NewsLetterForm(request.POST)
         if form.is_valid():
-            print('valid')
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+
+            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient.save()
+
+            send_welcome_email(name,email)
+
+            HttpResponseRedirect('news_today')
     else:
         form = NewsLetterForm()
 
